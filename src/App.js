6d931d3fc88eb2577f2666f1d7b4b95eb2e65397
Vinith <vinith2003@gmail.com>
2023-12-23 23:07:09 +0530

    Revert "final structure"
    This reverts commit 1e28d01d91c4ae4773ab16af2b290d89219b1477.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// import Cpp from "./Blogs/Cpp/Cpp";
-// import Javascript from "./Blogs/Javascript/Javascript";
-// import Python from "./Blogs/Python/Python";
-// import Card from "./components/Card";
-// import Header from "./components/Header";
+import Cpp from "./Blogs/Cpp/Cpp";
+import Javascript from "./Blogs/Javascript/Javascript";
+import Python from "./Blogs/Python/Python";
+import Card from "./components/Card";
+import Header from "./components/Header";
 // import Posts from "./components/Posts";
 
 export default function App() {
